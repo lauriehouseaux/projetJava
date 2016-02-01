32e45cbef9fd2441526bbb1ac950bc60c9501d72
COT Bastien <COT Bastien>
2016-02-01 11:18:27 +0100

    attributs de class

--- visualisationObjetsGit/src/Tree.java
+++ visualisationObjetsGit/src/Tree.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,6 @@
  *
  * @author COT
  */
-public class Tree {
-    
+public class Tree extends GitObject{
+    private ArrayList<String> Childs=new ArrayList();
 }
