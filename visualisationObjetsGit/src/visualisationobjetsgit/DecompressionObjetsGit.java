2b8399b397b159b48b08b2ab5d2fab77a458c5fd
lauriehouseaux <lma.houseaux@gmail.com>
2016-02-15 11:34:39 +0100

    code pour la decompression des objets git

--- visualisationObjetsGit/src/visualisationobjetsgit/DecompressionObjetsGit.java
+++ visualisationObjetsGit/src/visualisationobjetsgit/DecompressionObjetsGit.java
@@ -5,6 +5,8 @@
  */
 package visualisationobjetsgit;
 import java.io.*;
+import java.util.zip.DataFormatException;
+import java.util.zip.Inflater;
 /**
  *
  * @author Laurie
@@ -14,29 +16,36 @@ public class DecompressionObjetsGit {
 
 
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws DataFormatException{
 		FileInputStream fis;
-		int byteLu;
+		int octetLu;
 		try{
 			//creation d'un flux d'entrée ayant pour source un fichier nommé 			
 			//source,cette instanciation peut lever une exception de type 				
-			//FileNotFound
 			fis=new FileInputStream("C:\\Users\\Laurie\\Desktop\\essaiGit\\.git\\objects\\dc\\106e69bee157bb70ba5cb1f6b2933550ce6199");
+                        // initialisation d'un tableau d'octet
+                        byte[] b = new byte[30];
+                        // placement de chaque octet lu dans le tableau
+                        octetLu = fis.read(b);
+                        
+                        //System.out.println(octetLu);
+                    
 			//lecture des données
-			try{
-				long startChrono=System.currentTimeMillis();
-				System.out.println("debut lecture");
-				while((byteLu=fis.read())!=-1){
-                                    System.out.print(byteLu);
-                                }
-                                    
-				System.out.println("fin lecture");
-				System.out.println("durée="+(System.currentTimeMillis()-startChrono));
-			}
-			finally{	
-				//fermeture du flux
-				fis.close();
-			}
+			Inflater decompresser = new Inflater();
+                        // decompression du fichier par rapport a la taille de notre tableau
+                        decompresser.setInput(b,0,octetLu);
+                        // initialisation d'un tableau d'octet
+                        byte[] res = new byte[100];
+                        //decompression du fichier + recuperation de la taille du tableau 
+                        int taille = decompresser.inflate(res);
+                        decompresser.end();
+                        
+                        // affichage du contenu du tableau
+                        for (int i = 0; i < taille ; i++) {
+                        System.out.print((char)res[i]);
+                    }
+                        
+                        
 		}
 		catch(FileNotFoundException ef){
 			System.out.println("fichier introuvable");
