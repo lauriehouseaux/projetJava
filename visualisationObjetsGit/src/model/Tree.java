5b6419e781a8c88f95a27eecf7d7185fffe72761
lauriehouseaux <lma.houseaux@gmail.com>
2016-03-30 10:25:32 +0200

    ajout de quelques commentaires

--- visualisationObjetsGit/src/model/Tree.java
+++ visualisationObjetsGit/src/model/Tree.java
@@ -14,19 +14,20 @@ public class Tree extends GitObject{
         super(_file);
         
         
-//        System.out.println("-------------------------------------------------------");
-//        System.out.println("nom : " + this.getName());
-//        System.out.println(stringValue( FileReading.ReadFile(_file) ));
-//        System.out.println("-------------------------------------------------------");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("nom : " + this.getName());
+        System.out.println(stringValue( FileReading.ReadFile(_file) ));
+        System.out.println("-------------------------------------------------------");
 
     }
-    
+    // lecture de l'arbre de façon lisible pour un humain
      public static String stringValue( Byte[] inflated ) {
         
         StringBuilder content = new StringBuilder();
         
         int i = 0;
         char c;
+        // affichage de l'entete
         do {
             c = (char)inflated[i].byteValue();
             content.append(c);
@@ -34,7 +35,9 @@ public class Tree extends GitObject{
         }while (c != '\0');
         content.append('\n');
         
+        // pour chaque entree on fait la meme chose
         while(i < inflated.length) {
+            // affichage du nom
             do {
                 c = (char)inflated[i].byteValue();
                 content.append(c);
@@ -42,6 +45,7 @@ public class Tree extends GitObject{
             }while (c != '\0');
             content.append(" ");
             
+            // recuperation de la cle des fichiers que l'arbre contient
             for(int j = 0; j<20; j++){
                 content.append(String.format("%02x", inflated[i]));
                 i++;
