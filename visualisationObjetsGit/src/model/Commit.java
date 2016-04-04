189e7be97afc712304f11d03c5d9b772f41d1d6b
Jarretier Adrien <jarretier.adrien@gmail.com>
2016-04-04 23:33:15 +0200

    recherche des infos uniquement a la selection avec la methode fill()

--- visualisationObjetsGit/src/model/Commit.java
+++ visualisationObjetsGit/src/model/Commit.java
@@ -9,10 +9,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 
 public class Commit extends GitObject{
+    
+    private boolean filled;
     
     private Tree tree;
     private ArrayList< Commit > parents;
@@ -27,112 +31,159 @@ public class Commit extends GitObject{
     
     private String message;
    
-    public Commit(File _file, Git _gitInstance) throws IOException {
+    public Commit(File _file, Git _gitInstance) {
         
         super(_file, _gitInstance);
         
-        parents = new ArrayList<>();
+        this.filled = false;
+
+    }
+    
+    private void fill() throws IOException { 
         
-        String content = FileReading.stringValue( FileReading.ReadFile(_file) );
+        if ( !this.filled ) {
         
-//        System.out.println("-------------------------------------------------------");
-        
-        StringReader sr = new StringReader( content );
-        BufferedReader bf = new BufferedReader( sr );
-        
-        String line = bf.readLine();
-          
-        tree = (Tree)gitInstance.find( line.split(" ")[2] );
-        
-        line = bf.readLine();
-        
-        int i;
-        
-        while( ! line.isEmpty() )
-        {
-            switch( line.charAt(0)) {
-             
-                case 'p':
-                    parents.add( (Commit)gitInstance.find( line.split(" ")[1] ) );
-                    break;
-                    
-                case 'a':
-                    String[] wordsAuthorLine = line.split(" ");
-                    
-                    authorName = wordsAuthorLine[1];
-                    
-                    i=2;
-                    while( !wordsAuthorLine[i].startsWith("<") ) {
-                        
-                        authorName += ' '+wordsAuthorLine[i];
-                        i++;
-                    }
-//                    System.out.println("a : " + authorName);
-                    
-                    authorMail = wordsAuthorLine[i++].replaceAll("[<>]", "");
-//                    System.out.println("am : " + authorMail);
-                    
-                    dateWritten = wordsAuthorLine[i++]+" "+wordsAuthorLine[i];
-//                    System.out.println("dw : " + dateWritten);
-                    break;
-                    
-                case 'c':
-                    String[] wordsCommitterLine = line.split(" ");
-                    
-                    committerName = wordsCommitterLine[1];
-                    
-                    i=2;
-                    while( !wordsCommitterLine[i].startsWith("<") ) {
-                        
-                        committerName += ' '+wordsCommitterLine[i];
-                        i++;
-                    }
-//                    System.out.println("c : " + committerName);
-                    
-                    committerMail = wordsCommitterLine[i++].replaceAll("[<>]", "");
-//                    System.out.println("cm : " + committerMail);
-                    
-                    dateCommitted = wordsCommitterLine[i++]+" "+wordsCommitterLine[i];
-//                    System.out.println("dc : " + dateCommitted);
-                    break;    
-                
+            parents = new ArrayList<>();
+
+            String content = FileReading.stringValue( FileReading.ReadFile( this.getFile() ) );
+
+
+            StringReader sr = new StringReader( content );
+            BufferedReader bf = new BufferedReader( sr );
+
+            String line = bf.readLine();
+
+            tree = (Tree)gitInstance.find( line.split(" ")[2] );
+
+            line = bf.readLine();
+
+            int i;
+
+            while( ! line.isEmpty() )
+            {
+                switch( line.charAt(0)) {
+
+                    case 'p':
+                        parents.add( (Commit)gitInstance.find( line.split(" ")[1] ) );
+                        System.out.println( line.split(" ")[1] );
+                        break;
+
+                    case 'a':
+                        String[] wordsAuthorLine = line.split(" ");
+
+                        authorName = wordsAuthorLine[1];
+
+                        i=2;
+                        while( !wordsAuthorLine[i].startsWith("<") ) {
+
+                            authorName += ' '+wordsAuthorLine[i];
+                            i++;
+                        }
+
+                        authorMail = wordsAuthorLine[i++].replaceAll("[<>]", "");
+
+                        dateWritten = wordsAuthorLine[i++]+" "+wordsAuthorLine[i];
+                        break;
+
+                    case 'c':
+                        String[] wordsCommitterLine = line.split(" ");
+
+                        committerName = wordsCommitterLine[1];
+
+                        i=2;
+                        while( !wordsCommitterLine[i].startsWith("<") ) {
+
+                            committerName += ' '+wordsCommitterLine[i];
+                            i++;
+                        }
+
+                        committerMail = wordsCommitterLine[i++].replaceAll("[<>]", "");
+
+                        dateCommitted = wordsCommitterLine[i++]+" "+wordsCommitterLine[i];
+                        break;    
+
+                }
+
+                line = bf.readLine();
             }
 
-//            System.out.println("");
-            line = bf.readLine();
-        }
-        
-        StringBuilder messageBuilder = new StringBuilder();
-        while( line != null ) {
-            
-            messageBuilder.append( line ).append("\n");
-            line = bf.readLine();
-        }
-        
-        message = messageBuilder.toString();
-//        System.out.println(message);
-        
-//        System.out.println("-------------------------------------------------------");
+            StringBuilder messageBuilder = new StringBuilder();
+            while( line != null ) {
 
-    }   
+                messageBuilder.append( line ).append("\n");
+                line = bf.readLine();
+            }
+
+            message = messageBuilder.toString();
+            
+            this.filled = true;
+            
+        }
+        
+    }
 
     @Override
     public String toString() {
-    
-        StringBuilder sb = new StringBuilder( "name : " + getName() );
-        sb.append("tree : ").append(tree.getName());
-        parents.stream().forEach((parent) -> {
-            sb.append("parent : ").append(parent.getName());
-        });
-        sb.append("authorName : ").append(authorName);
-        sb.append("authorMail : ").append(authorMail);
-        sb.append("dateWritten : ").append(dateWritten);
-        sb.append("committerName : ").append(committerName);
-        sb.append("committerMail : ").append(committerMail);
-        sb.append("dateCommitted : ").append(dateCommitted);
-        sb.append("message : ").append(message);
         
-        return sb.toString();
+        try {
+
+            this.fill();
+            
+            StringBuilder sb = new StringBuilder( "name : " + getName() );
+            sb.append( System.getProperty("line.separator") );
+            sb.append( System.getProperty("line.separator") );
+            
+            sb.append("tree : ");
+            if( tree == null ) {
+                sb.append( "null" );
+            }
+            else {
+                sb.append(tree.getName());
+            }
+            sb.append( System.getProperty("line.separator") );
+            sb.append( System.getProperty("line.separator") );
+            
+            parents.stream().forEach((parent) -> {
+                sb.append("parent : ");
+                if( parent == null ) {
+                    sb.append( "null" );
+                }
+                else {
+                    sb.append(parent.getName());
+                }
+                sb.append( System.getProperty("line.separator") );
+            });
+            sb.append( System.getProperty("line.separator") );
+            
+            sb.append("authorName : ").append(authorName);
+            sb.append( System.getProperty("line.separator") );
+            
+            sb.append("authorMail : ").append(authorMail);
+            sb.append( System.getProperty("line.separator") );
+            
+            sb.append("dateWritten : ").append(dateWritten);
+            sb.append( System.getProperty("line.separator") );
+            sb.append( System.getProperty("line.separator") );
+            
+            sb.append("committerName : ").append(committerName);
+            sb.append( System.getProperty("line.separator") );
+            
+            sb.append("committerMail : ").append(committerMail);
+            sb.append( System.getProperty("line.separator") );
+            
+            sb.append("dateCommitted : ").append(dateCommitted);
+            sb.append( System.getProperty("line.separator") );
+            sb.append( System.getProperty("line.separator") );
+            
+            sb.append("message : ").append(message);
+            
+            return sb.toString();
+
+        } catch (IOException ex) {
+            
+            return ex.getMessage();
+            
+        }
     
     }
 
